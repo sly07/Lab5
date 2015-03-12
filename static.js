cf79a6e9866884ab007acd8ee3f921ea213018e6
Mike Sly <ec2-user@ip-172-31-36-130.us-west-2.compute.internal>
2015-03-12 19:57:36 +0000

    Pushing files from Amazon Server

--- static.js
+++ static.js
@@ -6,7 +6,53 @@ http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
 //Install Route for getcity
   //console.log(urlObj);
-  if(urlObj.pathname.indexOf("getcity") != -1){
+  if(urlObj.pathname == "/comment"){
+	//console.log("comment route");
+	if(req.method === "POST"){
+	  console.log("POST comment route");
+	  var jsonData = "";
+      req.on('data', function (chunk) {
+        jsonData += chunk;
+      });
+      req.on('end', function () {
+        var reqObj = JSON.parse(jsonData);
+        console.log(reqObj);
+        console.log("Name: "+reqObj.Name);
+        console.log("Comment: "+reqObj.Comment);
+var MongoClient = require('mongodb').MongoClient;
+        MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+          if(err) throw err;
+          db.collection('comments').insert(reqObj,function(err, records) {
+            console.log("Record added as "+records[0]._id);
+});
+          });
+        });
+	res.writeHead(200);
+	res.end();
+	}else if(req.method ==="GET"){
+	// console.log("In GET");
+//Read all of the database entries and return them in a JSON array
+      var MongoClient = require('mongodb').MongoClient;
+      MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+        if(err) throw err;
+        db.collection("comments", function(err, comments){
+          if(err) throw err;
+          comments.find(function(err, items){
+            items.toArray(function(err, itemArr){
+         //     console.log("Document Array: ");
+         //     console.log(itemArr);
+	res.writeHead(200);
+	res.end(JSON.stringify(itemArr));
+
+            });
+          });
+        });
+      });
+
+	}
+
+  }
+  else if(urlObj.pathname.indexOf("getcity") != -1){
 	//console.log("In Getcity");
   fs.readFile("cities.dat.txt", function(err,data){
 	if(err) throw err;
